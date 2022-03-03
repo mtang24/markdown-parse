a5b68143a0f273ae42d14ab20febf00966baddcb
mtang24 <michaeljtang18@gmail.com>
2022-03-02 20:15:56 -0800

    snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -19,13 +19,13 @@ public class MarkdownParseTest {
         assertEquals("test file 1", list1, MarkdownParse.getLinks(contents));
     }
 
-    @Test
-    public void ParseTest2() throws IOException {
-        Path fileName = Path.of("test-file2.md");
-	    String contents = Files.readString(fileName);
-        List<String> list2 = List.of("https://duckduckgo.com");
-        assertEquals("test file 2", list2, MarkdownParse.getLinks(contents));
-    }
+    // @Test
+    // public void ParseTest2() throws IOException {
+    //     Path fileName = Path.of("test-file2.md");
+	//     String contents = Files.readString(fileName);
+    //     List<String> list2 = List.of("https://duckduckgo.com");
+    //     assertEquals("test file 2", list2, MarkdownParse.getLinks(contents));
+    // }
 
     @Test
     public void ParseTest3() throws IOException {
@@ -39,12 +39,12 @@ public class MarkdownParseTest {
     //     assertEquals(5, MarkdownParse.subtract(11, 6)); 
     // }
 
-    @Test
-    public void testSpaceAfterParen() {
-        String contents = "[title]( space-in-url.com)";
-        List<String> expect = List.of("space-in-url.com");
-        assertEquals(expect, MarkdownParse.getLinks(contents));
-    }
+    // @Test
+    // public void testSpaceAfterParen() {
+    //     String contents = "[title]( space-in-url.com)";
+    //     List<String> expect = List.of("space-in-url.com");
+    //     assertEquals(expect, MarkdownParse.getLinks(contents));
+    // }
 
     // @Test
     // public void ParseTest4() throws IOException {
@@ -54,5 +54,28 @@ public class MarkdownParseTest {
     //     assertEquals("test file 6", list4, MarkdownParse.getLinks(contents));
     // }
 
+    // @Test
+    // public void testSnippet1() throws IOException {
+    //     Path fileName = Path.of("test-snippet1.md");
+	//     String contents = Files.readString(fileName);
+    //     List<String> snippet1 = List.of("google.com");
+    //     assertEquals("snippet 1", snippet1, MarkdownParse.getLinks(contents));
+    // }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("test-snippet2.md");
+	    String contents = Files.readString(fileName);
+        List<String> snippet2 = List.of("a.com, a.com(()), example.com");
+        assertEquals("snippet 2", snippet2, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("test-snippet3.md");
+	    String contents = Files.readString(fileName);
+        List<String> snippet3 = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals("snippet 3", snippet3, MarkdownParse.getLinks(contents));
+    }
 
 }
